7563f6e85f4b1265a86f79a7c44ab8b2cf5b7c03
vedant dandge <vedantdandgepro@gmail.com>
2022-10-12 03:34:39 +0530

    Title changed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1 className="brand">Moviehub</h1>
+      <h1 className="brand">MovieBuzz</h1>
 
       <div className="search">
         <input
